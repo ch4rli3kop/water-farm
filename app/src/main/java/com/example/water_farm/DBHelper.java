19eea6d6747571f68a6d43944947d3dabe49792d
ch4rli3kop <pch2180@gmail.com>
2021-10-02 08:21:25 +0900

    Modify Details

--- app/src/main/java/com/example/water_farm/DBHelper.java
+++ app/src/main/java/com/example/water_farm/DBHelper.java
@@ -11,7 +11,7 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class DBHelper extends SQLiteOpenHelper {
-    public static final int DATABASE_VERSION = 4;
+    public static final int DATABASE_VERSION = 5;
     public static final String DATABASE_NAME = "DeviceData.db";
 
     public DBHelper(Context context) {
